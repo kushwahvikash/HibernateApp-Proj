6a80e223ca6eece6ecd4dd64a3911a89cd429174
DELL <DELL@vikaskush>
2020-04-27 23:07:00 +0530

    modify

--- HAS-A_OneToMany(Cascading)/src/main/java/com/nt/model/Project.java
+++ HAS-A_OneToMany(Cascading)/src/main/java/com/nt/model/Project.java
@@ -2,6 +2,15 @@ package com.nt.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
 
 @Entity
 @Table(name="prjtab")
